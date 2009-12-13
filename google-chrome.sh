3a14c033a4cd25fb4b2106dd4dc681672973a381
Elan Ruusamäe <glen@pld-linux.org>
2009-12-13 12:13:30 +0000

    - it's chrome this time
    Changed files:
        google-chrome.sh -> 1.2

--- google-chrome.sh
+++ google-chrome.sh
@@ -11,7 +11,7 @@ export LD_LIBRARY_PATH=@libdir@${LD_LIBRARY_PATH:+:"$LD_LIBRARY_PATH"}
 # for to find xdg-settings
 export PATH=@libdir@${PATH:+:"$PATH"}
 
-# chromium needs /dev/shm being mounted
+# chrome needs /dev/shm being mounted
 m=$(awk '$2 == "/dev/shm" && $3 == "tmpfs" {print}' /proc/mounts)
 if [ -z "$m" ]; then
 	cat >&2 <<-'EOF'
