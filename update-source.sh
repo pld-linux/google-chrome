4187e1175d943876ca4e7c1c17eb7a724d21b3fc
Elan Ruusamäe <glen@delfi.ee>
2013-09-19 01:33:05 +0300

    primary.gz contains garbage at the end. ignore if resulting file was not 0 byte

--- update-source.sh
+++ update-source.sh
@@ -33,7 +33,7 @@ t=$(mktemp)
 repodata=primary-$branch-$(date +%Y%m%d).xml
 test -e $repodata || {
 	wget -c $sourceurl/repodata/primary.xml.gz
-	gzip -dc primary.xml.gz > $repodata
+	gzip -dc primary.xml.gz > $repodata || test -s $repodata
 }
 perl -ne 'm{<name>google-'$product-$branch'</name>} and m{<version epoch="0" ver="([\d.]+)" rel="(\d+)"/>} and print "$1 $2"' > $t < $repodata
 
